6d0f17fd5fb7e610fe041c560f0f03aca407c54f
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-06-30 22:19:26 -0600

    testing heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
 const mysql = require("mysql");
 
 
-// const conn = mysql.createConnection({
-//     CLEARDB_DATABASE_URL: "mysql://bd63f616e45530:d4f09c32@us-cdbr-east-02.cleardb.com/heroku_f5d05644ad099ad?reconnect=true",
-// });
-
 const conn = mysql.createConnection({
-  host     : 'us-cdbr-east-02.cleardb.com',
-  user     : 'bd63f616e45530',
-  password : 'd4f09c32',
-  database : 'heroku_f5d05644ad099ad',
-  port: process.env.PORT
+    CLEARDB_DATABASE_URL: "mysql://bd63f616e45530:d4f09c32@us-cdbr-east-02.cleardb.com/heroku_f5d05644ad099ad?reconnect=true",
 });
+
+// const conn = mysql.createConnection({
+//   host     : 'us-cdbr-east-02.cleardb.com',
+//   user     : 'bd63f616e45530',
+//   password : 'd4f09c32',
+//   database : 'heroku_f5d05644ad099ad'
+// });
 
 conn.connect((err) => {
   if (err) {
